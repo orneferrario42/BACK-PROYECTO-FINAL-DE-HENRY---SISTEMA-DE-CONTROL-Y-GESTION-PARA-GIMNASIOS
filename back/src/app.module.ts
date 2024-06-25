3035139c253a8f77367952fe4cc64ce00d9efc9d
jose <jose.freggiaro@gmail.com>
2024-06-25 16:49:02 +0200

    scaffolding

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { FileModule } from './file/file.module';
+import { ProfesorModule } from './profesor/profesor.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, AuthModule, FileModule, ProfesorModule],
   controllers: [AppController],
   providers: [AppService],
 })
