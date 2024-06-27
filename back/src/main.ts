bc9a573e4ca3be397b1d1d1da3eab831564a990a
stimado9021 <quinteroenrrique321@gmail.com>
2024-06-27 15:46:42 -0500

    poniendo al dia

--- back/src/main.ts
+++ back/src/main.ts
@@ -3,8 +3,10 @@ import { AppModule } from './app.module';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
+  const app = await NestFactory.create(AppModule,{
+    cors:true
+  });
+ 
  
   await app.listen(3000);
 }
