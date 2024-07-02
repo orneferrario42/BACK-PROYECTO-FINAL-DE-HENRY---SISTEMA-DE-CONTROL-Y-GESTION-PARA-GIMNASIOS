9948b5f2fcae15e8218e7ffb6aae0261d8640d95
stimado9021 <quinteroenrrique321@gmail.com>
2024-07-02 08:55:46 -0500

    actualizar repositorio local 3

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,7 +5,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { auth } from 'express-openid-connect';
 import { config as auth0Config } from './config/auth0.config';
 import { loggerGlobal } from './middleware/logger.middleware';
-import { bootstrap } from './bootstrap';
+//import { bootstrap } from './bootstrap';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
