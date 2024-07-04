074821846adb2d517ab2b01b614078426a740d67
unknown <jperdomo@iegabo.edu.co>
2024-07-04 01:53:42 -0500

    CRUD de plan

--- back/src/plan/plan.module.ts
+++ back/src/plan/plan.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PlanService } from './plan.service';
 import { PlanController } from './plan.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Plan } from './entities/plan.entity';
+import { planRepository } from './plan.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Plan])],
   controllers: [PlanController],
-  providers: [PlanService],
+  providers: [PlanService, planRepository],
 })
 export class PlanModule {}
--- back/src/plan/plan.repository.ts
+++ back/src/plan/plan.repository.ts
@@ -1,1 +1,32 @@
-export class planRepository{}
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Plan } from './entities/plan.entity';
+import { Repository } from 'typeorm';
+import { CreatePlanDto } from './dto/create-plan.dto';
+import { UpdatePlanDto } from './dto/put-plan.dto';
+
+@Injectable()
+export class planRepository {
+  constructor(
+    @InjectRepository(Plan) private planRepository: Repository<Plan>,
+  ) {}
+  async createPlan(createPlanDto: CreatePlanDto) {
+    const plan = this.planRepository.create(createPlanDto);
+    return await this.planRepository.save(plan);
+  }
+  async getPlanById(id: string) {
+    const plan = await this.planRepository.findOne({ where: { id } });
+    if (!plan) throw new NotFoundException(`Plan with id ${id} not found`);
+    return plan;
+  }
+  async updatePlan(id: string, updatePlanDto: UpdatePlanDto) {
+    await this.planRepository.update(id, updatePlanDto);
+    const updatePlan = await this.planRepository.findOneBy({ id });
+    if (!updatePlan)
+      throw new NotFoundException(`Plan with id ${id} not found `);
+    return updatePlan;
+  }
+  async getAllPlans() {
+    return await this.planRepository.find();
+  }
+}
