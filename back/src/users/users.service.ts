a2e269b6a429d4a13166d50da9b0bb9f9f7f0dc5
orneferrario42 <orneferrario@hotmail.com>
2024-07-15 21:01:54 -0300

    nada x2

--- back/src/users/users.service.ts
+++ back/src/users/users.service.ts
@@ -110,9 +110,6 @@ export class UsersService {
         'rutina',
       ],
     });
-    if (!users) {
-      throw new NotFoundException('Usuario no encontrado');
-    }
     const start = (page - 1) * limit;
     const end = start + limit;
     users = users.slice(start, end);
