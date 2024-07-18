a763696b7699fad4e7993f7340749a5536cdba46
orneferrario42 <orneferrario@hotmail.com>
2024-07-18 01:15:41 -0300

    EST VEZ SI PERRO ESPERO

--- back/src/main.ts
+++ back/src/main.ts
@@ -37,8 +37,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3001);
 }
 
 bootstrap();
